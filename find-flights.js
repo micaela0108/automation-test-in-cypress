b4a64463b6494bb87ddecd267040fee67e76bc94
micaela0108 <122472918+micaela0108@users.noreply.github.com>
2023-03-07 17:50:02 +0100

    Update find-flights.js

--- find-flights.js
+++ find-flights.js
@@ -1,17 +1,24 @@
 describe('search ticket', () => {
-  it('find the flight', () => {
-    cy.visit('https://www.vueling.com/es')
-    cy.get('#onetrust-accept-btn-handler').click();
+  beforeEach(() => {
+    cy.visit('https://www.vueling.com/es') // visit the page
+  })
+it('find the flight', () => {
+    cy.get('#onetrust-accept-btn-handler').click(); // accept the cookies
+    cy.wait(4000); // wait 4 seconds
+    cy.get('#originInput').click(); //click on input -- search by id
     cy.wait(4000);
-    cy.get('#originInput').click();
+    cy.get('div.searchbar-popup_header_search input[placeholder="Ciudad, país o aeropuerto"]').type('Madrid'); // type in the searchbar the word 'Madrid' for the origin
+    cy.get('p.list-item').contains('Madrid').click(); // click on "Madrid"
     cy.wait(4000);
-    cy.get('div.searchbar-popup_header_search input[placeholder="Ciudad, país o aeropuerto"]').type('Madrid'); 
-    cy.get('p.list-item').contains('Madrid').click(); 
+    cy.get('.searchbar-popup_header_search').click(); // click on the destiny searchbar
+    cy.get('p.list-item').contains('Alicante').click(); // click on "Alicante" in the list
     cy.wait(4000);
-    cy.get('.searchbar-popup_header_search').click();
-    cy.get('p.list-item').contains('Alicante').click();
-    cy.wait(4000);
-    //cy.get('a.ui-state-default.searcbar-datepicker-day-prices--red').click();
-    //cy.get('button.mv_button').contains('Aceptar').click();
+    cy.get('#calendarDaysTable2023324[data-month="3"][data-year="2023"]').click() // select the 24-04-2023 for the start date -- search by id
+    cy.wait(4000)//;
+    cy.get('#calendarDaysTable2023329[data-month="3"][data-year="2023"]').click() // select the 29-04-2023 for the end date -- search by id
+    cy.get('button.mv_button').contains('Aceptar').click(); 
+    cy.get('#btnSubmitHomeSearcher').click();
   })
 })
+
+/
